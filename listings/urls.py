1a767ba7608704e39066a951d56a44b19659371a
ali-py3 <man.pyyy@gmail.com>
2021-10-19 11:49:53 -0400

    Search box class base views

--- listings/urls.py
+++ listings/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 
 from . import views
 # app_name = "lister"
+from .views import SearchResultsView
 urlpatterns = [
     path('', views.index, name='listings'),
     path('<int:listing_id>', views.listing, name='listing'),
     # path('<slug:slug>', views.category, name='category'),
     # path('<slug:slug>/page/<int:page>', views.category, name='category'),
-    path('search', views.search, name='search'),
+    path('search/', SearchResultsView.as_view(), name='search_results'),
 ]
